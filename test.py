2bdf84f3dac03a626d2c4f274fd94556fb57529f
manik-sharma98 <45903492+manik-sharma98@users.noreply.github.com>
2020-02-10 13:07:34 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,20 @@
+arra = "12345678909877654321"
+print(arra[10:])
+print(arra[:10]+arra[10:])
+a = 10
+a += 10
+print(a)
+x = set()
+x.add(1)
+print(x)
+if 1>2:
+    print ("ok")
+elif 2>3:
+    print ("ok")
+else:
+    print ("not ok")
+class abc:
+    def name():
+        print ("hey")
+a= abc
+abc.name()
