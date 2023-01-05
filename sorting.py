b142becb0884d7c3eab0a4bd08003b2efa48680f
manik-sharma98 <prinjamanik7@gmail.com>
2023-01-06 00:44:56 +0530

    Added bubble sort in sorting

--- sorting.py
+++ sorting.py
@@ -1,10 +1,19 @@
 l = [1,2,3,4]
 l.sort(reverse=True)
-print(l)
+#print(l)
 
 string_list = ['abcd','abc','abcdef','abcde']
 string_list.sort(key = len)
-print(string_list)
+#print(string_list)
 
 strr = 'bcdaef'
-print(sorted(strr))
+#print(sorted(strr))
+
+nums = [1,10,4,5,7,2,9,8,3]
+
+#Bubble_sort
+for i in range(len(nums)):
+    for j in range(i,len(nums)):
+        if nums[i] > nums[j]:
+            nums[i],nums[j] = nums[j],nums[i]
+print(nums)
