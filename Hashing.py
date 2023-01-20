e1f1349a6149e586fd783fc6a4edd9584efe820b
manik-sharma98 <prinjamanik7@gmail.com>
2023-01-21 00:01:26 +0530

    Solved hash_map problems

--- Hashing.py
+++ Hashing.py
@@ -1,1 +1,44 @@
 #chaining
+class MyHash():
+    def __init__(self,b) -> None:
+        self.Bucket = b
+        self.Table = [[] for i in range(b)]
+    
+    def insert(self,x):
+        i = x % self.Bucket
+        self.Table[i].append(x)
+
+    def remove(self,x):
+        i = x % self.Bucket
+        if x in self.Table[i]:
+            self.Table[i].remove(x)
+    
+    def search(self,x):
+        i = x % self.Bucket
+        return x in self.Table[i]
+h = MyHash(7)
+h.insert(70)
+h.insert(71)
+h.insert(9)
+h.insert(56)
+h.insert(72)
+print(h.search(56))
+h.remove(56)
+print(h.search(56))
+h.remove(56)
+
+def count_disctnt(nums):
+    hash_map = {}
+    for n in nums:
+        if n in hash_map:
+            hash_map[n] += 1
+        else:
+            hash_map[n] = 1
+    res = 1
+    for i in range(1,len(nums)):
+        if nums[i] not in nums[0:i]:
+            res += 1
+    #return res
+    return len(hash_map.keys())
+
+print(count_disctnt([1,2,3,3,4,5,6,7,8,9,1]))
--- backtracking.py
+++ backtracking.py
@@ -15,6 +15,22 @@ def subsequences():
     back_track(nums,0,curr_start)
     return res
 
+def subsequences_string():
+    res = set()
+    
+    def back_track(nums,index,curr_start):
+        res.add("".join(curr_start))
+        for i in range(index,len(nums)):
+            if i > index and nums[i] == nums[i-1]:
+                continue
+            curr_start.append(nums[i])
+            back_track(nums,i+1,curr_start)
+            curr_start.pop()
+        
+    curr_start = []
+    nums = "abc"
+    back_track(nums,0,curr_start)
+    return res
 	
 	
 print(subsequences())
