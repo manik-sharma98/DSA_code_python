e0e76731a3866b316f52e78fd41c1b73b1797b6d
manik-sharma98 <prinjamanik7@gmail.com>
2023-02-03 00:07:27 +0530

    Traversal of tree

--- tree.py
+++ tree.py
@@ -3,3 +3,42 @@ class Node:
         self.left = None
         self.right = None
         self.val = val 
+
+#Traversal :->
+# --- Breath first (level order) (iterative)
+# --- Depth first (recursive/iterative)
+    # Preorder -> R,L,R
+    # Inorder -> L,R,R
+    # Postorder -> L,R,R
+
+# O(n) and space - O(H)
+def preorder(root):
+    if root != None:
+        print(root.val, end = ' ')
+        preorder(root.left)
+        preorder(root.right)
+
+def inorder(root):
+    if root != None:
+        inorder(root.left)
+        print(root.val, end = ' ')
+        inorder(root.right)
+
+def postorder(root):
+    if root != None:
+        postorder(root.left)
+        postorder(root.right)
+        print(root.val, end = ' ')
+    
+        
+root = Node(10)
+root.left = Node(20)
+root.right = Node(30)
+root.right.left = Node(40)
+root.right.right = Node(50)
+
+preorder(root)
+print()
+inorder(root)
+print()
+postorder(root)
