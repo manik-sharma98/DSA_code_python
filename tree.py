5057a535468b1098b6cea87c1f65c5e5f365dd3d
manik-sharma98 <prinjamanik7@gmail.com>
2023-02-03 18:57:32 +0530

    Solved bfs,dfs,max and height of tree

--- tree.py
+++ tree.py
@@ -1,3 +1,5 @@
+import math
+from collections import deque
 class Node:
     def __init__(self,val) -> None:
         self.left = None
@@ -38,6 +40,62 @@ def height(root):
     h = 1 + max(lh,rh)
     return h
     
+def print_k_distance(root,k):
+    if root == None:
+        return 0
+    if k == 0:
+        print(root.val,end=' ')
+    else:
+        print_k_distance(root.left,k-1)
+        print_k_distance(root.right,k-1)
+    
+def bfs(root):
+    q = deque()
+    q.append(root)
+    while q:
+        node = q.popleft()
+        print(node.val,end = ' ')
+        if node.left:
+            q.append(node.left)
+        if node.right:
+            q.append(node.right)
+def dfs(root):
+    current = root
+    stack = []
+    while True:
+        if current:
+            stack.append(current)
+            current = current.left
+        elif stack:
+            current = stack.pop()
+            print(current.val,end=' ')
+            current = current.right
+        else:
+            break
+def max_val(root):
+    q = deque()
+    q.append(root)
+    max_value = -math.inf
+    while q:
+        node = q.popleft()
+        max_value = max(node.val,max_value)
+        if node.left:
+            q.append(node.left)
+        if node.right:
+            q.append(node.right)
+    return max_value
+def size(root):
+    q = deque()
+    q.append(root)
+    size = 0
+    while q:
+        node = q.popleft()
+        size += 1
+        if node.left:
+            q.append(node.left)
+        if node.right:
+            q.append(node.right)
+    return size
 root = Node(10)
 root.left = Node(20)
 root.right = Node(30)
@@ -54,3 +112,16 @@ print('Postorder of binary tree -> ')
 postorder(root)
 print()
 print('Height of binary tree is ->',height(root))
+print()
+print('Height of binary tree is ->',height(root))
+print('Nodes from k distance is ->')
+print_k_distance(root,2)
+print()
+print('BFS->')
+bfs(root)
+print()
+print('DFS->')
+dfs(root)
+print()
+print('Maximum value in tree is ->',max_val(root))
+print('Size tree is ->',size(root))
