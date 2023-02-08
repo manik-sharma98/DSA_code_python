5785cce03288e96566d4c7c1e11b7a372b769d2e
manik-sharma98 <prinjamanik7@gmail.com>
2023-02-08 17:47:27 +0530

    Solved topo sort using dfs

--- Data structure/Graph/Topological_sort.py
+++ Data structure/Graph/Topological_sort.py
@@ -32,6 +32,21 @@ class TP_sort():
             print('Cycle Found')
         else:
             return res
+    
+    def dfs_untill(self,v,visited,stack):
+        visited[v] = True
+        for u in self.graph[v]:
+            if visited[u] == False:
+                self.dfs_untill(u,visited,stack)
+        stack.append(v)
+    def dfs_topo_sort(self):
+        visited = [False] * self.V
+        stack = []
+
+        for i in range(self.V):
+            if visited[i] == False:
+               self.dfs_untill(i,visited,stack)
+        return(stack[::-1])
 
 g = TP_sort(6)
 g.addEdge(5, 2);
@@ -43,3 +58,5 @@ g.addEdge(3, 1);
 
 print ("Following is a Topological Sort of the given graph")
 print(g.topological_sort())
+print ("Following is a Topological Sort of the given graph - DFS")
+print(g.dfs_topo_sort())
