eac19da3cfdf009d6f5a9879cef24cac46c0a2a1
manik-sharma98 <prinjamanik7@gmail.com>
2023-06-09 03:04:27 +0530

    Solved solved sum equals to target using dp

--- Algorithms/DP/Subsequence_DP/Subset Sum Equals to Target.py
+++ Algorithms/DP/Subsequence_DP/Subset Sum Equals to Target.py
@@ -3,10 +3,37 @@ def subset_sum(i,nums,k,dp):
         return 1
     if i == 0:
         return arr[0] == k
+    if dp[i][k] != False:
+        return dp[i][k]
     pick = subset_sum(i-1,nums,k-nums[i],dp)
+    
     not_pick = subset_sum(i-1,nums,k,dp)
-    return pick or not_pick
+    dp[i][k] =pick or not_pick 
+    return dp[i][k]
+arr = [1,2,3,4]
+k = 10
+n = len(arr) 
+dp = [[False]*(k+1) for _ in range(n)]
+print(subset_sum(len(arr)-1,arr,k,dp))
+
+def subset_sum_t(nums,target):
+    n = len(nums)
+    dp = [[False]*(k+1) for i in range(n)]
+    for i in range(n):
+        dp[i][0] = True
+    
+    if nums[0] <= target:
+        dp[0][nums[0]] = True
+    
+    for i in range(1,n):
+        for j in range(1,k+1):
+            not_taken = dp[i-1][j]
+            taken = 0
+            if nums[i] <= target:
+                taken = dp[i-1][j-nums[i]] 
+            dp[i][j] = not_taken or taken
+    return dp[n-1][k]
+
 arr = [1,2,3,4]
 k = 12
-print(subset_sum(len(arr)-1,arr,k,0))
-
+print(subset_sum_t(arr,k))
